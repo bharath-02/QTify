0933dd1652a8484c106dfd81117c51e9d714c5d5
Bharath <bharath@admins-MacBook-Pro-4.local>
2024-08-31 14:07:07 +0530

    init commit

---
+++ qtify/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>This is my website</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
